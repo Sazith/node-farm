78e81bda07ee353ce396ae937e6fc99b9d9a742f
Sazith <sazith.simecsoftdev@gmail.com>
2025-01-01 17:31:22 +0600

    api route added

--- http.js
+++ http.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
 
 //////////// SERVER
 const server = http.createServer((req, res) => {
@@ -12,6 +16,16 @@ const server = http.createServer((req, res) => {
     res.end('This is OVERVIEW'); 
   } else if (pathName === '/product') {
     res.end('This is PRODUCT');
+  }else if(pathName === '/api'){
+
+    // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) =>{
+    //   const productDate = JSON.parse(data)
+    //   console.log(productDate);
+    //   res.writeHead(200, {'Content-type': 'application/json'});
+    //   res.end(data)
+    // })
+    res.writeHead(200, {'Content-type': 'application/json'});
+    res.end(data)
   } else {
     res.writeHead(404, {
       'content-type':'text/html',
@@ -20,7 +34,7 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Page not found</h1>');
   }
 })
-
+  
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to request port 8000');
 
