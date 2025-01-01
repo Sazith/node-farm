15f6bc049660f749adca56fd493626fc806685da
Sazith <sazith.simecsoftdev@gmail.com>
2025-01-01 15:26:55 +0600

    error handle

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ console.log('File Written');
 // Non-blocking, Asynchronous way
 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if(err) return console.log('ERROR!');
   console.log(data1);
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
